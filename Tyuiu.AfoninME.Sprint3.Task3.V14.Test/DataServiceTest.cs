cdd347c89fc238b7169bf575123c6a8e859329fc
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-11-14 00:19:52 +0500

    task3

---
+++ Tyuiu.AfoninME.Sprint3.Task3.V14.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AfoninME.Sprint3.Task3.V14.Lib;
+
+namespace Tyuiu.AfoninME.Sprint3.Task3.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string Value = "ddd";
+            char replaceable = 'd';
+            char replacement = '’';
+            string res = ds.ReplaceCharInString(Value, replaceable, replacement);
+            string wait = "’’’";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AfoninME.Sprint3.Task3.V14/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.AfoninME.Sprint3.Task3.V14.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Афонин М.Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #14                                                             *");
+Console.WriteLine("* Выполнил: Афонин Максим Евгеньевич | ИСТНб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Значение:");
+string value = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Значение:");
+char replaceable = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Значение:");
+char replacement = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string res = ds.ReplaceCharInString(value, replaceable, replacement);
+
+Console.WriteLine(res);
+
+Console.ReadKey();
