f5deb50ad15a8faa45b714fb4ee432192976571f
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-11-14 02:39:09 +0500

    task6

---
+++ Tyuiu.AfoninME.Sprint3.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.AfoninME.Sprint3.Task6.V13.Lib;
+
+namespace Tyuiu.AfoninME.Sprint3.Task6.V13.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 8, stopValue = 17;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            Assert.AreEqual(117, res);
+        }
+    }
+}
