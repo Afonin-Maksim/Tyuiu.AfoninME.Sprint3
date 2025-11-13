758f1751efae94572dbbca4072792c06392d411d
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-11-13 23:02:31 +0500

    task1

---
+++ Tyuiu.AfoninME.Sprint3.Task1.V1.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.AfoninME.Sprint3.Task1.V1.Lib
+{
+    public class DataService : ISprint3Task1V1
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double multSeries = 1;
+            while (startValue <= stopValue)
+            {
+                multSeries = multSeries * (Math.Pow(3.0 / startValue, -2));
+                startValue++;
+            }
+            return Math.Round(multSeries, 3);
+        }
+    }
+}
