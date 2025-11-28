77277961e2a5ca8bd8a6b65583b6b5fb1c07034b
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-11-29 00:26:00 +0500

    task7

--- Tyuiu.AfoninME.Sprint3.Task7.V4.Test/DataServiceTest.cs
+++ Tyuiu.AfoninME.Sprint3.Task7.V4.Test/DataServiceTest.cs
@@ -15,7 +15,7 @@ namespace Tyuiu.AfoninME.Sprint3.Task7.V4.Test
 
             double[] result = ds.GetMassFunction(start, stop);
 
-            // Проверяем, что длина массива соответствует диапазону [-5; 5]
+            
             Assert.AreEqual(11, result.Length);
         }
     }
